0cf835c13a5b376b15847d317919cb8f82548895
PushEax <pusheax.vk@gmail.com>
2025-05-25 22:15:02 +0300

    add simple log in middleware

--- src/index.js
+++ src/index.js
@@ -4,6 +4,15 @@ const app = express();
 
 const PORT = 3000;
 
+app.use((req, res, next) => {
+  console.log('Time:', new Date().toLocaleString(), 'Request URL:', req.url);
+  next();
+});
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello World!' });
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
   console.log(`Link: http://localhost:${PORT}`);
