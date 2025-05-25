1cb87c86ffd91314996faec606e887aa2277b75e
PushEax <pusheax.vk@gmail.com>
2025-05-25 23:01:11 +0300

    move server code to file

--- src/index.js
+++ src/index.js
@@ -1,37 +1,3 @@
-import express from 'express';
-import pino from 'pino-http';
-import cors from 'cors';
+import { startServer } from './server.js';
 
-const PORT = 3000;
-
-const app = express();
-
-app.use(cors());
-
-app.use(pino({ transport: { target: 'pino-pretty' } }));
-
-// Вбудований у express middleware для обробки (парсингу) JSON-даних у запитах
-// наприклад, у запитах POST або PATCH
-app.use(express.json());
-
-app.get('/', (req, res) => {
-  res.json({ message: 'Hello World!' });
-});
-
-app.use((req, res, next) => {
-  res.status(404).json({
-    message: 'Not found',
-  });
-});
-
-// Middleware для обробких помилок (приймає 4 аргументи)
-app.use((err, req, res, next) => {
-  res.status(500).json({ message: 'Something went wrong!' });
-});
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-  console.log(`Link: http://localhost:${PORT}`);
-});
-
-export default app;
+startServer();
