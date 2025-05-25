7c6bdc488f8fad3e1ecdfc3a38cd1473d924d370
PushEax <pusheax.vk@gmail.com>
2025-05-25 22:07:35 +0300

    minimal express server app

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-const message = "Hello world";
+import express from 'express';
 
-console.log(message);
+const app = express();
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  console.log(`Link: http://localhost:${PORT}`);
+});
+
+export default app;
