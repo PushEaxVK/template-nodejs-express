e07f75d58b94066002aadec86d8bcb12894b8cb7
PushEax <pusheax.vk@gmail.com>
2025-05-25 21:33:54 +0300

    update eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,17 @@
-import js from "@eslint/js";
-import globals from "globals";
-import { defineConfig } from "eslint/config";
-
+import js from '@eslint/js';
+import globals from 'globals';
+import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.node } },
+  {
+    files: ['src/**/*.{js,mjs,cjs}'],
+    languageOptions: { globals: globals.node },
+    plugins: { js },
+    extends: ['js/recommended'],
+    rules: {
+      semi: 'error',
+      'no-unused-vars': ['error', { args: 'none' }],
+      'no-undef': 'error',
+    },
+  },
 ]);
