b4597ec107a38214153055dcc0cb612a32fed369
PushEax <pusheax.vk@gmail.com>
2025-05-25 22:21:25 +0300

    add middelware for errors

--- src/index.js
+++ src/index.js
@@ -17,6 +17,11 @@ app.get('/', (req, res) => {
   res.json({ message: 'Hello World!' });
 });
 
+// Middleware для обробких помилок (приймає 4 аргументи)
+app.use((err, req, res, next) => {
+  res.status(500).json({ message: 'Something went wrong!' });
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
   console.log(`Link: http://localhost:${PORT}`);
