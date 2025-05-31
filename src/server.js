6ea6ea4a2581fdcdd8be86d74fc2883ded11a288
PushEax <pusheax.vk@gmail.com>
2025-05-31 09:23:01 +0300

    add error message

--- src/server.js
+++ src/server.js
@@ -33,7 +33,10 @@ export const startServer = () => {
   });
 
   app.use((err, req, res, next) => {
-    res.status(500).json({ message: 'Something went wrong!' });
+    res.status(500).json({
+      message: 'Something went wrong!',
+      error: err.message,
+    });
   });
 
   app.listen(PORT, () => {
