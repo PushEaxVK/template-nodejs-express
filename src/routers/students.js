cfaa63afef4117a67ad177615778695b0df9b3c7
PushEax <pusheax.vk@gmail.com>
2025-06-12 00:19:24 +0300

    remove ctrlWrapper

--- src/routers/students.js
+++ src/routers/students.js
@@ -7,15 +7,14 @@ import {
   patchStudentController,
   upsertStudentController,
 } from '../controllers/students.js';
-import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 
 const router = Router();
 
-router.get('/students', ctrlWrapper(getStudentsController));
-router.get('/students/:studentId', ctrlWrapper(getStudentByIdController));
-router.post('/students', ctrlWrapper(createStudentController));
-router.delete('/students/:studentId', ctrlWrapper(deleteStudentController));
-router.put('/students/:studentId', ctrlWrapper(upsertStudentController));
-router.patch('/students/:studentId', ctrlWrapper(patchStudentController));
+router.get('/students', getStudentsController);
+router.get('/students/:studentId', getStudentByIdController);
+router.post('/students', createStudentController);
+router.delete('/students/:studentId', deleteStudentController);
+router.put('/students/:studentId', upsertStudentController);
+router.patch('/students/:studentId', patchStudentController);
 
 export default router;
