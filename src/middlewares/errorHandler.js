154664eceda2d9af9ab6caa65f6469c2bf36334e
PushEax <pusheax.vk@gmail.com>
2025-06-13 10:09:33 +0300

    add MongoDB error

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,10 +1,20 @@
 import { isHttpError } from 'http-errors';
+import { MongooseError } from 'mongoose';
 
 export const errorHandler = (err, req, res, next) => {
   if (isHttpError(err)) {
-    res.status(err.status).json({
+    return res.status(err.status).json({
       status: err.status,
       message: err.message,
+      data: err,
+    });
+  }
+
+  if (err instanceof MongooseError) {
+    return res.status(500).json({
+      status: 500,
+      message: 'MongoDB error',
+      details: err.message,
       data: err,
     });
   }
